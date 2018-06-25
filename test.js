b8382d5edbee8c499f14a7859d1a91f222e1f325
tw0517tw <tw0517tw@gmail.com>
2018-06-25 22:09:44 +0800

    log files and add badge to readme

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ const globby = require('globby')
 
 const files = globby.sync('./lib')
 
+console.log(files)
+
 if (files.length >= 2) {
   throw new Error('built 2 files or more');
   process.exit(1);
